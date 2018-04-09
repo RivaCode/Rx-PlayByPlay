46fbc9b4695565433b28a3c6f9924ab3787b32f2
Stas Rivkin <slavariv@gmail.com>
2018-04-09 22:41:38 +0300

    Switch timer impl

--- RxExamples/Pages/GoogleSearch/GoogleSearchPage.xaml.cs
+++ RxExamples/Pages/GoogleSearch/GoogleSearchPage.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
-using System.Reactive.Linq;
+using System.Threading;
 using System.Windows.Controls;
+using System.Windows.Threading;
+
 
 namespace RxExamples.Pages
 {
@@ -9,23 +11,61 @@ namespace RxExamples.Pages
     /// </summary>
     public partial class GoogleSearchPage : UserControl
     {
+        private CancellationTokenSource _cts;
+        private DispatcherTimer _throttleTimer;
+        private string _lastSearchToken;
+        private string _searchCandidate;
+
         public GoogleSearchPage()
         {
             InitializeComponent();
-            Observable.FromEventPattern(SearchBox, nameof(SearchBox.TextChanged))
-                .Select(_ => SearchBox.Text)
-                .Where(newSearchToken => newSearchToken.Length > 2)
-                .Throttle(TimeSpan.FromSeconds(0.5))
-                .DistinctUntilChanged()
-                .Select(searchToken => SearchService.SearchAsync(searchToken))
-                .Switch()
-                .ObserveOnDispatcher()
-                .Subscribe(result =>
-                {
-                    var (countries, stamp) = result;
-                    CountriesList.ItemsSource = countries;
-                    TimeBlock.Text = stamp.ToString();
-                });
+
+            SearchBox.TextChanged += OnSearchTextChanged;
+
+            _throttleTimer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(0.5)};
+            _throttleTimer.Tick += OnThrottleElapsed;
+        }
+
+        private void OnSearchTextChanged(object sender, TextChangedEventArgs textChangedEventArgs)
+        {
+            var candidateSearchTerm = SearchBox.Text;
+            if (candidateSearchTerm.Length < 3)
+            {
+                _throttleTimer.Stop();
+                return;
+            }
+
+            _searchCandidate = candidateSearchTerm;
+            RestartThrottle();
+        }
+
+        private async void OnThrottleElapsed(object sender, EventArgs elapsedEventArgs)
+        {
+            _throttleTimer.Stop();
+            if (_lastSearchToken == _searchCandidate)
+            {
+                return;
+            }
+
+            _cts?.Cancel(true);
+            _cts = new CancellationTokenSource();
+            try
+            {
+                _lastSearchToken = _searchCandidate;
+                var (countries, stamp) = await SearchService.SearchAsync(_lastSearchToken, _cts.Token);
+                CountriesList.ItemsSource = countries;
+                TimeBlock.Text = stamp.ToString();
+            }
+            catch
+            {
+                // ignore
+            }
+        }
+
+        private void RestartThrottle()
+        {
+            _throttleTimer.Stop();
+            _throttleTimer.Start();
         }
     }
 }
